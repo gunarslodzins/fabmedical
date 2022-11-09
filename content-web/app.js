07bc9323e0c0eb962028b65d8e8a3f7cad4bf249
gunarslodzins <gunars.lodzins@gmail.com>
2022-11-09 13:22:21 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("a646545d-db56-4d97-b8cc-ea46592dafb2");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
